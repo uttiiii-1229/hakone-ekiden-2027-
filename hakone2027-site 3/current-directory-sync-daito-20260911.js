3361f60d34f001d7ae01b9a203bdacad01c8a36d
uttiiii-1229 <usdkisk1229@gmail.com>
2026-09-14 17:18:24 +0900

    Load Kokugakuin and pending official PB audits

--- hakone2027-site 3/current-directory-sync-daito-20260911.js
+++ hakone2027-site 3/current-directory-sync-daito-20260911.js
@@ -31,7 +31,9 @@
 
 // Load official current-roster/PB audits before the athlete directories are built.
 if(typeof document!=='undefined' && document.readyState==='loading'){
+  document.write('<script src="current-pb-audit-kanagawa-20260913.js?v=20260913-audit1"><\/script>');
   document.write('<script src="current-pb-audit-soka-20260913.js?v=20260913-audit1"><\/script>');
   document.write('<script src="current-pb-audit-teikyo-20260914.js?v=20260914-audit1"><\/script>');
   document.write('<script src="current-pb-audit-nihon-20260914.js?v=20260914-audit1"><\/script>');
+  document.write('<script src="current-pb-audit-kokugakuin-20260914.js?v=20260914-audit1"><\/script>');
 }
