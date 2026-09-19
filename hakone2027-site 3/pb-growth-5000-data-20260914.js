d5c9a89003af24631e6db6b5476a7c6dec700b09
uttiiii-1229 <usdkisk1229@gmail.com>
2026-09-20 08:40:02 +0900

    Add verified Kusuoka high-school-to-current 5000m growth data

--- hakone2027-site 3/pb-growth-5000-data-20260914.js
+++ hakone2027-site 3/pb-growth-5000-data-20260914.js
@@ -7,5 +7,6 @@ window.pbGrowth5000Verified2026=[
   {team:'青山学院大学',name:'佐藤 愛斗',highSchoolPb:'13:57.15',source:'4years.（2024年度新入部員5000m PB）'},
   {team:'青山学院大学',name:'飯田 翔大',highSchoolPb:'13:34.20',source:'4years.（2024年度新入部員5000m PB）'},
   {team:'青山学院大学',name:'折田 壮太',highSchoolPb:'13:28.78',source:'4years.（2024年度新入部員5000m PB）'},
-  {team:'青山学院大学',name:'福冨 翔',highSchoolPb:'14:09.05',currentPb:'13:55.95',source:'4years.（須磨学園時代14:09.05）/ 駅伝ナビ2026（現PB13:55.95）'}
+  {team:'青山学院大学',name:'福冨 翔',highSchoolPb:'14:09.05',currentPb:'13:55.95',source:'4years.（須磨学園時代14:09.05）/ 駅伝ナビ2026（現PB13:55.95）'},
+  {team:'帝京大学',name:'楠岡 由浩',highSchoolPb:'13:55.84',currentPb:'13:26.92',source:'慶誠高校陸上競技部（高校PB13:55.84）/ 日本選手権2026（現PB13:26.92）'}
 ];
